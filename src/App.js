5dfd2930dc037bf4c5fef7f7f5a65df22b291a47
yakubu rufai mahatma <yrufai23@gmail.com>
2019-01-22 23:22:17 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     super(props);
     this.handleClear = this.handleClear.bind(this);
     this.state = {
-      input: ""
+      input: " "
     };
   }
   handleClear() {
